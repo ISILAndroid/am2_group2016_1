a4de2138b365889674e2d362d0c414a0c40ef133
Eduardo Medina <emedinaa@gmail.com>
2016-03-30 17:14:27 -0500

    feat(fragments): Ejemplo, agregar fragment mediante XML

--- Fragments/AM2Fragments/app/src/main/java/com/isil/fragments/MainActivity.java
+++ Fragments/AM2Fragments/app/src/main/java/com/isil/fragments/MainActivity.java
@@ -1,13 +1,15 @@
 package com.isil.fragments;
 
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.isil.fragments.view.fragments.OtherFragment;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity  implements OtherFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +26,10 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return false;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
