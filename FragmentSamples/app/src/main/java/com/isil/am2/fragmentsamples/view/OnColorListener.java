b81f334d3882af12b550c923c97a7e158f273fb0
Eduardo Medina <emedinaa@gmail.com>
2016-04-06 17:42:22 -0500

    feat(FragmentSample): Ejemplos fragments

---
+++ FragmentSamples/app/src/main/java/com/isil/am2/fragmentsamples/view/OnColorListener.java
@@ -0,0 +1,7 @@
+package com.isil.am2.fragmentsamples.view;
+
+/**
+ * Created by emedinaa on 09/09/15.
+ */
+public interface OnColorListener {
+}
---
+++ FragmentSamples/app/src/main/java/com/isil/am2/fragmentsamples/view/OnFragmentListener.java
@@ -0,0 +1,9 @@
+package com.isil.am2.fragmentsamples.view;
+
+/**
+ * Created by emedinaa on 25/08/15.
+ */
+public interface OnFragmentListener {
+
+    void onSendMessage(String msg);
+}
