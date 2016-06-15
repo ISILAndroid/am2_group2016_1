43b1a7f14851ed657f67acc3a4c214c137f987da
Eduardo Medina <emedinaa@gmail.com>
2016-06-15 11:10:30 -0500

    sample nav drawer

---
+++ NavigationDrawer/app/src/main/java/com/example/android/navigationdrawerexample/OnNavigationListener.java
@@ -0,0 +1,8 @@
+package com.example.android.navigationdrawerexample;
+
+/**
+ * Created by em on 15/06/16.
+ */
+public interface OnNavigationListener {
+
+}
---
+++ NavigationDrawer/app/src/main/java/com/example/android/navigationdrawerexample/fragments/AFragment.java
@@ -0,0 +1,103 @@
+package com.example.android.navigationdrawerexample.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.android.navigationdrawerexample.OnNavigationListener;
+import com.example.android.navigationdrawerexample.R;
+
+import java.util.Locale;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * to handle interaction events.
+ * Use the {@link AFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    public static final String ARG_PLANET_NUMBER = "planet_number";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnNavigationListener mListener;
+
+    public AFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AFragment newInstance(String param1, String param2) {
+        AFragment fragment = new AFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_a, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_planet, container, false);
+        int i = getArguments().getInt(ARG_PLANET_NUMBER);
+        String planet = getResources().getStringArray(R.array.planets_array)[i];
+
+        int imageId = getResources().getIdentifier(planet.toLowerCase(Locale.getDefault()),
+                "drawable", getActivity().getPackageName());
+        ((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);
+        getActivity().setTitle(planet);
+        return rootView;
+    }
+
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnNavigationListener) {
+            mListener = (OnNavigationListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+}
